de46a1d5562605f8b09f85621ef7c2870bbdfb69
mohit <mohitthakur9901@gmail.com>
2024-03-30 14:20:25 +0530

    reinstalled node_modules

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,12 +1,14 @@
-"use client";
 
 import { useBalance } from "@repo/store/balance";
 
 export default function Page(): JSX.Element {
   const balance = useBalance();
+
+
   return (
    <div className="bg-blue-500">
-         hi there {balance}
+                hi there {balance}
+
    </div>
   );
 }
